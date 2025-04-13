69f057cf4f4a2bd89a1fec559ab808bc20ced3dc
Boru Isako <isco.jaldy@gmail.com>
2025-04-13 17:42:07 +0300

    Added item.js that contains data being displayed in the tables

---
+++ src/data/items.js
@@ -0,0 +1,9 @@
+const expenses = [
+  { id: 1, expense: "Ugali Matumbo", description: "Monday's Lunch", category: "Food", amount: 200, date: "2025-04-07" },
+  { id: 2, expense: "KPLC tokens", description: "Power tokens", category: "Utilities", amount: 5000, date: "2025-04-12" },
+  { id: 3, expense: "Sneakers", description: "Black sports sneakers for gym", category: "Footwear", amount: 3500, date: "2025-04-14" },
+  { id: 4, expense: "Oraimo Spacebuds", description: "Wireless earbuds", category: "Electronics", amount: 4500, date: "2025-04-13" },
+];
+
+  export default expenses;
+  
